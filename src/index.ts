452af5f8e06d7eae9a2c21f7a5be40d1d105340e
seb-sep <59621473+seb-sep@users.noreply.github.com>
2024-03-28 18:59:33 -0400

    Initial commit
    Generated by create-expo-module 0.6.7.

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+
+// Import the native module. On web, it will be resolved to WhisperKitExpo.web.ts
+// and on native platforms to WhisperKitExpo.ts
+import WhisperKitExpoModule from './WhisperKitExpoModule';
+import WhisperKitExpoView from './WhisperKitExpoView';
+import { ChangeEventPayload, WhisperKitExpoViewProps } from './WhisperKitExpo.types';
+
+// Get the native constant value.
+export const PI = WhisperKitExpoModule.PI;
+
+export function hello(): string {
+  return WhisperKitExpoModule.hello();
+}
+
+export async function setValueAsync(value: string) {
+  return await WhisperKitExpoModule.setValueAsync(value);
+}
+
+const emitter = new EventEmitter(WhisperKitExpoModule ?? NativeModulesProxy.WhisperKitExpo);
+
+export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
+  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+}
+
+export { WhisperKitExpoView, WhisperKitExpoViewProps, ChangeEventPayload };
