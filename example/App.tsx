0c43f20290039e46a692df596fe4f9c00c71aff2
seb-sep <59621473+seb-sep@users.noreply.github.com>
2024-05-09 02:24:34 -0400

    cleanup example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import * as fs from 'expo-file-system';
+import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 import { TranscriberInitializer, transcribe } from 'whisper-kit-expo';
@@ -8,24 +7,25 @@ export default function App() {
 
   const [transcription, setTranscription] = useState("");
   const [path, setPath] = useState("");
-  
+
+
   return (
     <TranscriberInitializer>
       <View style={styles.container}>
-        <Text>Enter (absolute) filepath to transcribe here:</Text>
+        <Text>Enter (absolute) filepath for audio to transcribe:</Text>
         <TextInput
           style={styles.textBox}
           value={path}
           onChangeText={setPath}
           multiline
           numberOfLines={4}
-          placeholder='absolute filepath'
+          placeholder="path here"
           autoCapitalize='none'
         />
         <Button
-          title='Transcribe'
+          title='Transcribe path'
           onPress={async () => {setTranscription(await transcribe(path))}}
-          />
+        />
         <Text>Transcription from WhisperKit is:</Text>
         <Text>{transcription}</Text>
       </View>
